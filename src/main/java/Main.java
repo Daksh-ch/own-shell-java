5a6d540e9def31d7e8d291c10d291beb194ba1fe
Daksh Chaudhary <dakshchaudhary@Dakshs-MacBook-Air.local>
2026-02-26 14:41:52 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,18 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        String command = "";
-        do{
+        
+        while(true){
             System.out.print("$ ");
 
             Scanner sc = new Scanner(System.in);
-            command = sc.nextLine();
-        
+            String command = sc.nextLine();
+
+            if(command.equals("exit")){
+                break;
+            }
             System.out.println(command + ": command not found");
         }
-        while(!command.equals("exit"));
         
-
+        sc.close();
     }
+
 
 }
