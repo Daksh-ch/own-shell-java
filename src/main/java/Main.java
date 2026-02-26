a73d175766e51160a0f9afdd9997ea3a9e3483a6
Daksh Chaudhary <dakshchaudhary@Dakshs-MacBook-Air.local>
2026-02-26 14:30:13 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.print("$ ");
+
+        Scanner sc = new Scanner(System.in);
+        String command = sc.nextLine();
+    
+        System.out.println(command + ": command not found");
+
     }
+
 }
