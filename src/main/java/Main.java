35f7ca8a3a70e64b00ce1ecf4ab1bd2ceade8107
Daksh Chaudhary <dakshchaudhary@Dakshs-MacBook-Air.local>
2026-02-27 14:01:10 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,35 +19,54 @@ public class Main {
             String fw = command.indexOf(" ") == -1 ? command : command.substring(0,command.indexOf(' '));
             String rem = command.indexOf(" ") == -1 ? "" : command.substring(command.indexOf(' ') + 1);
 
-            switch (fw) {
-                case "exit":
-                    System.exit(0);
-                case "type":
-                    if(rem.equals("type") || (rem.equals("echo")) || (rem.equals("exit"))){
-                    System.out.println(rem + " is a shell builtin");
-                    }
-                    else{
-                        Boolean found = false;
-                        for(String dir : dirs){
-                            String path = dir + File.separator + rem;
-                            Path fullPath = Path.of(path);
-                            if(Files.exists(fullPath)){
-                                if(Files.isExecutable(fullPath)){
-                                    found = true;
-                                    System.out.println(rem + " is " + fullPath);
-                                    break;
-                                }
+
+            
+
+
+            
+            if(fw.equals("exit")) System.exit(0);
+            else if(fw.equals("type")){
+                if(rem.equals("type") || (rem.equals("echo")) || (rem.equals("exit"))){
+                System.out.println(rem + " is a shell builtin");
+                }
+                else{
+                    Boolean found = false;
+                    for(String dir : dirs){
+                        String path = dir + File.separator + rem;
+                        Path fullPath = Path.of(path);
+                        if(Files.exists(fullPath)){
+                            if(Files.isExecutable(fullPath)){
+                                found = true;
+                                System.out.println(rem + " is " + fullPath);
+                                break;
                             }
                         }
-                        if(found == false) System.out.println(rem + ": not found");  
                     }
-                    break;
-                case "echo":
-                    System.out.println(rem);
-                    break;
-                default:
-                    System.out.println(fw + ": command not found");
+                    if(found == false) System.out.println(rem + ": not found");  
+                }
             }
+            else if(fw.equals("echo")){
+                System.out.println(rem);
+            }
+            else if(fw != null){
+                String[] input = command.split(" ");
+                ProcessBuilder pb = new ProcessBuilder(input);
+
+                pb.redirectErrorStream(true);
+                Process process = pb.start();
+
+                //read the output of the process and also show any error messages
+                Scanner processScanner = new Scanner(process.getInputStream());
+                while(processScanner.hasNextLine()){
+                    System.out.println(processScanner.nextLine());  
+                
+
+            }
+            
+            else System.out.println(fw + ": command not found");
+            
+
+            
             
         }
         
