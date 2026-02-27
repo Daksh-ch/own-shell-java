1c4159fa6e6763f21c6fffcbfac147530eb92a61
Daksh Chaudhary <dakshchaudhary@Dakshs-MacBook-Air.local>
2026-02-27 13:06:12 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,15 @@
+import java.io.File;
+import java.nio.file.Files;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         
         Scanner sc = new Scanner(System.in);
+
+        String allPath = System.getenv("PATH");
+        String[] dirs = allPath.split(File.pathSeparator);
+
         
         while(true){
             System.out.print("$ ");
@@ -12,22 +18,35 @@ public class Main {
             String fw = command.indexOf(" ") == -1 ? command : command.substring(0,command.indexOf(' '));
             String rem = command.indexOf(" ") == -1 ? "" : command.substring(command.indexOf(' ') + 1);
 
-            if(fw.equals("exit")) break;
-            else if(fw.equals("type")){
-                if(rem.equals("type") || (rem.equals("echo")) || (rem.equals("exit"))){
+            switch (fw) {
+                case "exit":
+                    break;
+                case "type":
+                    if(rem.equals("type") || (rem.equals("echo")) || (rem.equals("exit"))){
                     System.out.println(rem + " is a shell builtin");
-                }
-                else System.out.println(rem + " not found");  
+                    }
+                    else{
+                        for(String dir : dirs){
+                            String fullPath = dir + File.separator + rem;
+                            if(Files.exists(fullPath)){
+                                if(Files.isExecutable(fullPath)){
+                                    System.out.println(rem + " is " + fullPath);
+                                }
+                            }
+                            else System.out.println(rem + ": not found");
+                        }
+                    }
+                    // else System.out.println(rem + " not found");
+                    break;
+                case "echo":
+                    System.out.println(rem);
+                    break;
+                default:
+                    System.out.println(fw + ": command not found");
             }
-            else if(fw.equals("echo")){
-                System.out.println(rem);
-            }
-            else{
-                System.out.println(fw + ": command not found");
-            }
+            
         }
-
-        sc.close();
+        
     }
 
 
