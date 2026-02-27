256461e35f5d3f28881b7013811ae83c7375129d
Daksh Chaudhary <dakshchaudhary@Dakshs-MacBook-Air.local>
2026-02-27 10:44:51 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,19 +9,15 @@ public class Main {
             System.out.print("$ ");
             String command = sc.nextLine();
 
-            String fw = command.indexOf(" ") == -1 ? command : command.substring(command.indexOf(0,' '));
-            String rem = command.indexOf(" ") == -1 ? command : command.substring(' ' + 1);
+            String fw = command.indexOf(" ") == -1 ? command : command.substring(command.indexOf(0,command.indexOf(' ')));
+            String rem = command.indexOf(" ") == -1 ? "" : command.substring(' ' + 1);
 
-            if(fw.equals("type")){
+            if(fw.equals("exit")) break;
+            else if(fw.equals("type")){
                 if(rem.equals("type") || (rem.equals("echo")) || (rem.equals("exit"))){
                     System.out.println(rem + " is a shell builtin");
                 }
-                else{
-                    System.out.println(rem + " not found");
-                }
-            }
-            else if(fw.equals("exit")){
-                break;
+                else System.out.println(rem + " not found");  
             }
             else if(fw.equals("echo ")){
                 System.out.println(rem);
@@ -29,7 +25,6 @@ public class Main {
             else{
                 System.out.println(fw + ": command not found");
             }
-
         }
 
         sc.close();
